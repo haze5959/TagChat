76241e6a8e471af9daec1404010a7c69a082bdc6
OQ <haze5959@naver.com>
2016-09-02 17:24:46 +0900

    0902
    test용 소켓 채팅방 구현

---
+++ io.js
@@ -0,0 +1,13 @@
+module.exports = function(server){
+  var io = require('socket.io')(server);
+
+  io.on('connection', function(socket){
+      socket.on('message', function(msg){
+        console.log(msg);
+        socket.emit('my message', msg);
+        socket.broadcast.emit('other message', msg);
+    });
+  });
+
+  return io;
+}
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'TagChat 설계' });
 });
 
+
+
+
+//임시 채팅방 테스트
+router.get('/test', function(req, res, next) {
+  res.render('dalkom_talk', { title: '1:1 채팅방' });
+});
+
 module.exports = router;

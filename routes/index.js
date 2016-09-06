6ea2871cb96b48d8d42ed4eb30a0e4d55703c64c
OQ <haze5959@naver.com>
2016-09-06 17:37:06 +0900

    1:1채팅방 연결 미완료
    연결하는 법 모르겠다

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TagChat 설계' });
+  console.log("get 들어왔다");
+
+  console.log(req.query.tagName);
+  if(req.query.tagName  == "1:1")
+  {
+    console.log("성공!!");
+    // fs.readFile('dalkom_talk',function(error, data){
+    //   res.writeHead(200, {'Content-Type': 'text/html'});
+    //   res.end(data);
+    // });
+
+    //res.render('dalkom_talk', { title: '1:1 채팅방' });
+  }
+  else
+  {
+    res.render('index', { title: 'TagChat 설계' });
+  }
 });
 
 
 
 
-//임시 채팅방 테스트
-router.get('/test', function(req, res, next) {
-  res.render('dalkom_talk', { title: '1:1 채팅방' });
-});
+// //임시 채팅방 테스트
+// router.get('/test', function(req, res, next) {
+//   res.render('dalkom_talk', { title: '1:1 채팅방' });
+// });
+
+
 
 module.exports = router;

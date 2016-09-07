1a374690f7a1d1f9bf76eefa75eab3908fe62e5b
OQ <haze5959@naver.com>
2016-09-07 19:05:41 +0900

    페이지 이동 성공
    이제는 1:1 채팅방 만들자

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
+//var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("get 들어왔다");
 
-  console.log(JSON.parse(req.query));
   if(req.query.tagName  == "1:1")
   {
-    console.log("성공!!");
-    //res.redirect('index');
-    //res.location('http://localhost:3000/');
-    //res.redirect('http://google.com');
-    // fs.readFile('dalkom_talk',function(error, data){
-    //   res.writeHead(200, {'Content-Type': 'text/html'});
-    //   res.end(data);
-    // });
-
-    //res.render('dalkom_talk', { title: '1:1 채팅방' });
+    res.render('dalkom_talk', { title: '1:1 채팅방' });
   }
   else
   {
@@ -34,6 +24,9 @@ router.get('/test', function(req, res, next) {
   res.render('dalkom_talk', { title: '1:1 채팅방' });
 });
 
+router.get('/test2', function(req, res, next) {
+  res.render('index_origin', { title: '실험실험' });
+});
 
 
 module.exports = router;

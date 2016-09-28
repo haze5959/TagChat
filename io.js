640112bf6faedf7c3021c79f299cdf8c4e5785ac
OQ <haze5959@naver.com>
2016-09-28 17:32:23 +0900

    댓글 효과 추가1
    효과 좀더 추가하자

--- io.js
+++ io.js
@@ -2,10 +2,11 @@ module.exports = function(server){
   var io = require('socket.io')(server);
 
   io.on('connection', function(socket){
-      socket.on('message', function(msg){
-        console.log(msg);
-        socket.emit('my message', msg);
-        socket.broadcast.emit('other message', msg);
+    socket.on('message', function(msg){
+      console.log(msg.content + " 하고 " + msg.color);
+
+      socket.emit('my message', msg);
+      socket.broadcast.emit('other message', msg);
     });
   });
 

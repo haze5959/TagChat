7d2430a432b783e8f7b45608c668347fd3e45003
OQ <haze5959@naver.com>
2016-10-05 13:05:27 +0900

    버그 생겨서 다시 시작
    16/10/05

--- io.js
+++ io.js
@@ -3,8 +3,6 @@ module.exports = function(server){
 
   io.on('connection', function(socket){
     socket.on('message', function(msg){
-      console.log(msg.content + " 하고 " + msg.color);
-
       socket.emit('my message', msg);
       socket.broadcast.emit('other message', msg);
     });

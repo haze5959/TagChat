7c71b0620b183b06c8fe6fd341401a77a431b917
OQ <haze5959@naver.com>
2016-10-07 10:19:12 +0900

    dfsdf
    dfdf

--- io.js
+++ io.js
@@ -2,16 +2,13 @@ module.exports = function(server){
   var io = require('socket.io')(server);
 
   io.on('connection', function(socket){
-<<<<<<< HEAD
     socket.on('message', function(msg){
       socket.emit('my message', msg);
       socket.broadcast.emit('other message', msg);
-=======
       socket.on('message', function(msg){
         console.log(msg);
         socket.emit('my message', msg);
         socket.broadcast.emit('other message', msg);
->>>>>>> origin/master
     });
   });
 

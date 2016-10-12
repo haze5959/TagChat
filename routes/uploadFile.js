ce74302196959337c0eab01ecb979c6dd290182d
OQ <haze5959@naver.com>
2016-10-12 18:02:41 +0900

    그림 저장 일자별로 잘 안됨
    처음 폴더 만들고 넣는게 잘 안됨

--- routes/uploadFile.js
+++ routes/uploadFile.js
@@ -1,10 +1,30 @@
 //파일 업로드를 위한 js
 var multer = require('multer');
+var fs = require('fs');
+var dateFormat = require('dateformat');
 
 var storage =   multer.diskStorage({
-
   destination: function (req, file, callback) {
-    callback(null, './public/uploads');
+    //오늘 날짜 폴더 생성하고
+    var now = new Date();
+    var filePath = "./public/uploads";
+    now = dateFormat(now, "yyyy-mmmm-dS");
+    filePath = filePath + "/" + now;
+
+    var temp = fs.readdirSync(filePath, function (err, files)  {
+      if(!err){ //에러가 없다면
+        //폴더가 이미 존재하므로 아무것도 안하고 패스~
+      }else{
+        fs.mkdir(filePath, '0777', function(err2){
+        	if(err2){
+            console.log("폴더가 안만들어 짐");
+          }
+        	console.log('dir writed - ' + filePath);
+        });
+      }
+    });
+
+    callback(null, filePath);
   },
   filename: function (req, file, callback) {
 
@@ -19,5 +39,6 @@ var storage =   multer.diskStorage({
 });
 
 var uploadFile = multer({ storage : storage}).single('chatOneOne');
+
 
 module.exports = uploadFile;

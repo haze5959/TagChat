8bb5cb1b6dc024d5f671378123470cddaf6b353e
OQ <haze5959@naver.com>
2016-10-14 15:52:50 +0900

    1:1 채팅방 진짜 완성
    수고했다

--- io.js
+++ io.js
@@ -2,11 +2,22 @@ module.exports = function(server){
   var io = require('socket.io')(server);
 
   io.on('connection', function(socket){
+    //최초 접속
+    socket.on('talk chat_in', function(msg){
+      socket.broadcast.emit('talk chat_in', msg);
+    });
+
+    // //채팅방 나갔을 때
+    socket.on('disconnect', function(){
+      socket.broadcast.emit('talk chat_out');
+    });
+
+    //메시지 교환
     socket.on('message', function(msg){
       socket.emit('my message', msg);
       socket.broadcast.emit('other message', msg);
     });
-
+    //사진 올릴 때
     socket.on('ImgMessage', function(Img_Path){
       socket.emit('my ImgMessage', Img_Path);
       socket.broadcast.emit('other ImgMessage', Img_Path);
